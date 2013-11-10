2bcb1a631b1e83a951da1a90a367c729c1db9f69
SarinSangpongaumpai <sarin.s19@gmail.com>
2013-11-10 17:21:54 +0700

    FuncTree update2

--- FuncTree.java
+++ FuncTree.java
@@ -1,20 +1,31 @@
-// class FuncTree that will construct function tree with random height.
+// this class will construct tree function that will use to generate art picture
 public class FuncTree {
-    // set max and min level of tree
 	public  final int MAX_LEVEL = 8;
 	public  final int MIN_LEVEL = 4;
-	// local variable int height of tree
+    private FuncNode root;
 	private int height;
-    // constructor will create function tree;
+    
 	public FuncTree() {
 		constructTree();
 	}
-	// random height of tree and set to local variable
+	// height of tree will be in range MAX_LEVEL to MIN_LEVEL
     private void randomHeight(){
-    	this.height = (int)(Math.random()*MAX_LEVEL+ MIN_LEVEL);
+    	this.height = (int)(Math.random()*(MAX_LEVEL-MIN_LEVEL)+ MIN_LEVEL);
     }
-    // this method will use command to set height of tree and create leaf node in each level 
+    // random height of tree and set function in each node of tree
 	private void constructTree() {
         randomHeight();
+		FuncNode leaf = new FuncNode(null);
+		this.root = new FuncNode(leaf);
+		
+		for (int i = 0; i < this.height-1; i++) {
+			FuncNode node = new FuncNode(null);
+			leaf.setLeafNode(node);
+			leaf = node;
+		}
+	}
+	// return root node of tree .
+	public FuncNode getRoot(){
+		return this.root;
 	}
 }
