ae8c7dc7213a7b694b2fde9441041989ba15dfee
Salisa5528 <g.asilas_gift@hotmail.com>
2013-11-12 22:11:00 +0700

    enum class use to create function

--- FuncTree.java
+++ FuncTree.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // this class will construct tree function that will use to generate art picture
 public class FuncTree {
 	public  final int MAX_LEVEL = 8;
@@ -9,10 +10,25 @@ public class FuncTree {
 		constructTree();
 	}
 	// height of tree will be in range MAX_LEVEL to MIN_LEVEL
+=======
+public class FuncTree {
+	public  final int MAX_LEVEL = 8;
+	public  final int MIN_LEVEL = 4;
+	private int height;
+
+	public FuncTree() {
+		constructTree();
+	}
+	
+>>>>>>> parent of 6e3c8b9... FuncTree comment update
     private void randomHeight(){
     	this.height = (int)(Math.random()*(MAX_LEVEL-MIN_LEVEL)+ MIN_LEVEL);
     }
+<<<<<<< HEAD
     // random height of tree and set function in each node of tree
+=======
+    
+>>>>>>> parent of 6e3c8b9... FuncTree comment update
 	private void constructTree() {
         randomHeight();
 		FuncNode leaf = new FuncNode(null);
@@ -24,8 +40,16 @@ public class FuncTree {
 			leaf = node;
 		}
 	}
+<<<<<<< HEAD
 	// return root node of tree .
 	public FuncNode getRoot(){
 		return this.root;
 	}
 }
+=======
+	
+	public FuncNode getRoot(){
+		return this.root;
+	}
+}
+>>>>>>> parent of 6e3c8b9... FuncTree comment update
