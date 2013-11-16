7445f1600b049c9ef18c9065958742ebaab60aec
SarinSangpongaumpai <sarin.s19@gmail.com>
2013-11-16 18:56:11 +0700

    Update

--- FuncNode.java
+++ FuncNode.java
@@ -1,57 +1,30 @@
+// Sarin Sangpongaumpai fixed
 
-public class FuncNode {
-	private String expr; //expression
-	private FuncNode leafNode;
-	private Function func;//function
-	private final int NUMBER_FUNCTION = 2; //SIN and COS
-	private final int NUMBER_VARIABLE  = 2; // x and y
-	private int random;
+package Sarin;
+// this class will create node w
+ private String expr; //expression
+ private FuncNode leafNode;
+ p
+ }
+    // set leaf node and random function
+ public void setLeafNode(FuncNode leafNode) {
+  this.leafNode = leafNode;
+  random();
+ }
+ // random function in this class(node)
+ private void random() {
+  if (this.leafNode == null) {
+   this.random = (int) (Math.random() * NUMBER_VARIABLE + NUMBER_FUNCTION);
+  }
+  else {
+   this.random = (int) (Math.random() * NUMBER_FUNCTION);
+  }
+ }
+ // set and return value and calculate by each of expression 
+ public double calculate(double x, double y) {
+  switch (this.random) {
+  case 0:
+  
+ }
 
-	public FuncNode(FuncNode leafNode) {
-		setLeafNode(leafNode);
-	}
-
-	public void setLeafNode(FuncNode leafNode) {
-		this.leafNode = leafNode;
-		random();
-	}
-	
-	private void random() {
-		if (this.leafNode == null) {
-			this.random = (int) (Math.random() * NUMBER_FUNCTION + NUMBER_VARIABLE);
-		}
-		else {
-			this.random = (int) (Math.random() * NUMBER_FUNCTION);
-		}
-	}
-	
-	public double calculate(double x, double y) {
-		switch (this.random) {
-		case 0:
-			this.expr = func.SIN.toString()+"(PI*y" ;
-			return Math.sin(Math.PI * x * this.leafNode.calculate(x, y));
-		case 1:
-			this.expr = func.COS.toString()+"(PI*x" ;
-			return Math.cos(Math.PI * y * this.leafNode.calculate(x, y));
-		case 2:
-			this.expr = "x";
-			return x;
-		default:
-			this.expr = "y";
-			return y;
-		}
-	}
-    // return string of expression
-	public String funcToString() {
-		if (this.leafNode != null) {
-			return   this.expr +"*"+ this.leafNode.funcToString() + ")";
-		} else {
-			return "("+ this.expr +")";
-		}
-	}
-    // enum class use to create the function
-	private enum Function {
-		SIN, COS  ;
-	}
-
-}
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
--- FuncTree.java
+++ FuncTree.java
@@ -1,31 +1,23 @@
-// this class will construct tree function that will use to generate art picture
-public class FuncTree {
-	public  final int MAX_LEVEL = 8;
-	public  final int MIN_LEVEL = 4;
-    private FuncNode root;
-	private int height;
-    
-	public FuncTree() {
-		constructTree();
-	}
-	// height of tree will be in range MAX_LEVEL to MIN_LEVEL
-    private void randomHeight(){
-    	this.height = (int)(Math.random()*(MAX_LEVEL-MIN_LEVEL)+ MIN_LEVEL);
+// Sarin Sangpongaumpai fixed
+package Sarin;
+
+import java.util.Random;
+
+public class ExprBuilder {
+
+    public static final int MAX_LEVEL = 4;
+    public static final int MIN_LEVEL = 1;
+    private Random rand;
+    private int height;
+
+    public ExprBuilder() {
+        rand = new Random();
+        height = rand.nextInt();
+     {
+            ExprNode node = new ExprNode(null);
+            leaf.setNode(node);
+            leaf = node;
+        }
+        return root;
     }
-    // random height of tree and set function in each node of tree
-	private void constructTree() {
-        randomHeight();
-		FuncNode leaf = new FuncNode(null);
-		this.root = new FuncNode(leaf);
-		
-		for (int i = 0; i < this.height-1; i++) {
-			FuncNode node = new FuncNode(null);
-			leaf.setLeafNode(node);
-			leaf = node;
-		}
-	}
-	// return root node of tree .
-	public FuncNode getRoot(){
-		return this.root;
-	}
 }
